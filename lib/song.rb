2804a68f0f760ef3699f465cbd8bd769cfadf972
Justice Payne <spikepayne@yahoo.com>
2020-09-12 19:15:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,12 +48,7 @@ class Song
     song
   end
   
-  def song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    @@all << song
-    song
-  end
+  def 
   
 
 end
